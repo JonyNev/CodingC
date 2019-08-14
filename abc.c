150347f303adf94f464f13080ee294f9f8d8351f
JonyNev <54073446+JonyNev@users.noreply.github.com>
2019-08-14 13:15:22 +0300

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char c = 0 ;
+    scanf (" %c", &c);
+  
+      printf("new leter: %c",c-32);
+   
+    return 0;
+}
