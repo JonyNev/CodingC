86d728a24f2277fcd0b7e86eb31959674900ef00
JonyNev <54073446+JonyNev@users.noreply.github.com>
2019-08-19 10:49:47 +0300

    Update and rename dghyug.c to Last_num_printer.c

--- Last_num_printer.c
+++ Last_num_printer.c
@@ -1,9 +1,7 @@
 /******************************************************************************
-
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 
 
@@ -18,14 +16,19 @@ printf("How many numberes you want to tipe?");
   int  i,o=0;
   int res[N];
    int sal[Num_in];
-  if(Num_in>5){
+  if(Num_in>5)
+  {
  for ( i=0;i<Num_in;i++)
  {
     printf("input number %d:\n",i+1);
     scanf("%d",&sal[i]);
     if(i<Num_in-5){continue;}
    else{res[o]=sal[i];o++;}
-       printf("the last 5 numberes are:\n%d\n%d\n%d\n%d\n%d",res[0],res[1],res[2],res[3],res[4]); return 0;}}
+    if(i==Num_in-1){ printf("the last 5 numberes are:\n%d\n%d\n%d\n%d\n%d",res[0],res[1],res[2],res[3],res[4]); return 0;}
+     
+ }
+      
+  }
  for ( i=0;i<Num_in;i++)
  {
     printf("input number %d:\n",i+1);
@@ -37,9 +40,6 @@ printf("How many numberes you want to tipe?");
               case 3: printf("\n%d\n%d\n%d ",sal[0],sal[1],sal[2]); break;
                  case 2: printf("\n%d\n%d",sal[0],sal[1]); break;
                     case 1: printf("\n%d",sal[0]); break;}
-    }
-    }
-    
-             
+            
     return 0;
 }
