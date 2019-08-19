d288b9a099ebf848880f1c70577a32d50c078774
JonyNev <54073446+JonyNev@users.noreply.github.com>
2019-08-19 15:26:29 +0300

    Create vopjbhgk.c

---
+++ vopjbhgk.c
@@ -0,0 +1,81 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdbool.h>
+#include <stdio.h>
+#define N (10)
+int main()
+{
+int a[N];
+int b[N];
+int num=0;
+int counter1,counter2;
+ printf("input the sum number : ");
+ scanf("%d",&num);
+   for(int i=0;i<n;i++)
+   { 
+       printf("input number %d: ",i+1);
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {  
+       if(num>=0)
+          {
+             if(a[i]>num || a[i]<0)
+             {
+               int x=0;
+               b[x]=a[i];
+               x++;
+               x=counter1;
+             }
+               int x=0;
+               b[x]=a[i];
+               x++;
+               x=counter2;
+          }
+       else
+          {
+            if(a[i]<num || a[i]>0)
+            {
+               int x=0;
+               b[x]=a[i];
+               x++;
+               x=counter1;                     
+            }
+               int x=0;
+               b[x]=a[i];
+               x++;
+               x=counter2;
+          } 
+   }switch(counter1){
+       case 0: break;
+       case 1: if() break;
+       case 2: break;
+       case 3: break;
+       case 4: break;
+       case 5: break;
+       case 6: break;
+       case 7: break;
+       case 8: break;
+       case 9: break;
+   }
+   switch(counter2){
+       case 0: break;
+       case 1: break;
+       case 2: break;
+       case 3: break;
+       case 4: break;
+       case 5: break;
+       case 6: break;
+       case 7: break;
+       case 8: break;
+       case 9: break;
+   }
+   
+ return 0;
+}
